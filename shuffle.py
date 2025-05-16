f95e760dd898c726cd8a7f3faafd212f63d9f557
condongilbert <condongilbert@gmail.com>
2025-05-16 14:25:59 -0700

    first iteration of a shuffle python script

---
+++ shuffle.py
@@ -0,0 +1,28 @@
+import random
+import matplotlib.pyplot as plt
+
+# Define the deck
+suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+deck = [f"{rank} of {suit}" for suit in suits for rank in ranks]
+
+def shuffle_deck(deck):
+    shuffled = deck.copy()
+    random.shuffle(shuffled)
+    return shuffled
+
+def visualize_deck(deck, title='Shuffled Deck'):
+    plt.figure(figsize=(12, 6))
+    plt.axis('off')
+    for i, card in enumerate(deck):
+        x = i % 13
+        y = 3 - (i // 13)
+        plt.text(x, y, card, fontsize=9, ha='center', va='center', bbox=dict(boxstyle="round,pad=0.3", edgecolor='black', facecolor='white'))
+
+    plt.title(title, fontsize=16)
+    plt.tight_layout()
+    plt.show()
+
+# Shuffle and visualize
+shuffled_deck = shuffle_deck(deck)
+visualize_deck(shuffled_deck)
